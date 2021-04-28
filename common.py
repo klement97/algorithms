f57a8c663a737239e4aaf8855c24354f111fa6e2
Klement Omeri <klement.omeri@cardoai.com>
2021-04-29 00:29:49 +0200

    feat(common): add high precision math functions

---
+++ common.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+
+km_to_mile_ratio = 0.62137119223
+
+
+def dsum(*args) -> Decimal:
+    if not args:
+        raise ValueError('Missing required arguments')
+
+    return sum([Decimal(x) for x in args])
+
+
+def dprod(*args) -> Decimal:
+    if not args:
+        raise ValueError('Missing required arguments')
+
+    prod = Decimal(1)
+    for arg in args:
+        prod *= Decimal(arg)
+
+    return prod
+
+
+def ddiv(dividend, divisor) -> Decimal:
+    if not (dividend and divisor):
+        raise ValueError('Missing required arguments')
+
+    return Decimal(dividend) / Decimal(divisor)
