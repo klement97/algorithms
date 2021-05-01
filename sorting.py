02cf96cb5196ce2fcdc3a06457823d5b16b7971d
Klement Omeri <klementomeri97@gmail.com>
2021-05-01 12:54:37 +0200

    feat: add bubble sort

--- sorting.py
+++ sorting.py
@@ -1,6 +1,7 @@
-from common import swap_list
+from common import swap_list, timer
 
 
+@timer
 def selection_sort(lst: list) -> list:
     list_len = len(lst)
 
@@ -12,11 +13,24 @@ def selection_sort(lst: list) -> list:
     return lst
 
 
+@timer
 def insertion_sort(lst: list) -> list:
     for i in range(len(lst)):
         j = i
         while j > 0 and lst[j - 1] > lst[j]:
             swap_list(lst, j - 1, j)
             j -= 1
+
+    return lst
+
+
+@timer
+def bubble_sort(lst: list) -> list:
+    len_lst = len(lst)
+
+    for i in range(len_lst - 1):
+        for j in range(len_lst - i - 1):
+            if lst[j] > lst[j + 1]:
+                swap_list(lst, j, j + 1)
 
     return lst
