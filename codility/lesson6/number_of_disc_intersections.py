d8532142cc925b0a3cd41c1d9f6b4ffa2ce04f51
Klement Omeri <klementomeri97@gmail.com>
2021-11-27 18:39:43 +0100

    feat: add lesson6 answers

---
+++ codility/lesson6/number_of_disc_intersections.py
@@ -0,0 +1,58 @@
+"""
+We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
+
+We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
+
+The figure below shows discs drawn for N = 6 and A as follows:
+
+  A[0] = 1
+  A[1] = 5
+  A[2] = 2
+  A[3] = 1
+  A[4] = 4
+  A[5] = 0
+
+
+There are eleven (unordered) pairs of discs that intersect, namely:
+
+discs 1 and 4 intersect, and both intersect with all the other discs;
+disc 2 also intersects with discs 0 and 3.
+Write a function:
+
+def solution(A)
+
+that, given an array A describing N discs as explained above, returns the number of (unordered) pairs of intersecting discs.
+The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
+
+Given array A shown above, the function should return 11, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [0..2,147,483,647].
+"""
+
+
+def solution(array):
+    intersections = 0
+
+    for i in range(len(array) - 1):
+        center_i = i
+        radius_i = array[i]
+        start_i, end_i = center_i - radius_i, center_i + radius_i
+
+        for j in range(i + 1, len(array)):
+            center_j = j
+            radius_j = array[j]
+            start_j, end_j = center_j - radius_j, center_j + radius_j
+
+            if (start_j <= start_i <= end_j) or \
+                    (start_j <= end_i <= end_j) or \
+                    (start_i <= start_j <= end_i) or \
+                    (start_i <= end_j <= end_i):
+                intersections += 1
+
+        if intersections > 10000000:
+            return -1
+
+    return intersections
